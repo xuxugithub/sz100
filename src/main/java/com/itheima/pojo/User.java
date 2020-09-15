0352d1e350047ab8f0c70e23ea756be293eb46b1
admin <1149802487@qq.com>
2020-09-15 17:19:19 +0800

    提交工程到本地仓库

---
+++ src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,27 @@
+package com.itheima.pojo;
+
+/**
+ * @Author: xuyanwei
+ * @Date: 2020/9/15 10:33
+ */
+public class User {
+    private int age;
+    private  String usernmae;
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getUsernmae() {
+        return usernmae;
+    }
+
+    public void setUsernmae(String usernmae) {
+        this.usernmae = usernmae;
+    }
+}
